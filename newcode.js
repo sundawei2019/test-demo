9300469e5deaed46abc84235130273a1606a13d2
sundawei2018 <dsun2016@gmail.com>
2018-06-20 00:25:31 -0400

    confilit 2018 lenovo

--- newcode.js
+++ newcode.js
@@ -7,3 +7,5 @@ console.log("add this from sundawei2018");
 console.log("add this from 2019 new branch");
 
 console.log("continue this awesome testing add a line on lenovo account 2018");
+
+console.log("conflits lenovo 2018");
