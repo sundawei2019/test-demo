865ea6e573a370d3eae1fd7439cfa91159c524d9
sundawei2018 <dsun2016@gmail.com>
2018-06-19 23:10:28 -0400

    new commit from 2018 lenovo

--- newcode.js
+++ newcode.js
@@ -5,3 +5,5 @@ console.log("hello world");
 console.log("add this from sundawei2018");
 
 console.log("add this from 2019 new branch");
+
+console.log("continue this awesome testing add a line on lenovo account 2018");
